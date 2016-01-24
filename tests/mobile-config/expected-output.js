f8b6c665f1b20719a13bc145688a7547c80135f0
alex <alex028502@gmail.com>
2016-01-24 18:26:07 +0100

    get rid of final comma in generated code

--- tests/mobile-config/expected-output.js
+++ tests/mobile-config/expected-output.js
@@ -1,6 +1,5 @@
 //after generating images, stick this in mobile-config.js
 App.hypotheticalImageType({
     'image': 'resources/hypotheticalImageType/320x480.png',
-    'image_big': 'resources/hypotheticalImageType/640x960.png',
-    //actually remove the final comma from the above line - wish this were python
+    'image_big': 'resources/hypotheticalImageType/640x960.png'
 });
